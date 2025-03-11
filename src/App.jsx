c4c24e39ccf031cdd76637731696225465ce82f1
arbin-mahato <proarbind@gmail.com>
2025-03-11 14:52:31 +0530

    Created card component and designed homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Usercard from './components/usercard.jsx';
+import "./App.css";
+import Usercard from "./components/usercard.jsx";
 
 function App() {
-  const cards = Array.from({ length: 10 });
+  const cards = Array.from({ length: 8 });
   const gridStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 1fr)',
-    gap: '16px',
-    padding: '16px',
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gap: "16px",
+    padding: "16px",
   };
 
   return (
